89856a9f2f348dcbf1ee4293d5b5da6a6b718aa0
sarah.mwangi1@student.moringaschool.com <snmwangi88@gmail.com>
2026-04-25 23:09:44 +0300

    phase-1-tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevent page from refreshing on submit
+
+// Step 3: Get the task description from the form
+ const task = document.getElementById("new-task-description").value;
+
+// Call function
+  buildToDo(task);
+});
+
+// Step 4: Build and display the task
+function buildToDo(task) {
+  const taskList = document.getElementById("tasks");
+  const li = document.createElement("li");
+
+  li.textContent = task;
+
+  // Append the li to the task list
+  taskList.appendChild(li);
+}
 });
